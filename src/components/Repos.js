32efa435115ae425de04121aa5ed020e2700c1a3
Piotr Gaik <piotgie@gmail.com>
2022-01-11 23:11:15 +0100

    charts api

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,12 +5,50 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
 
-  const {repos} = React.useContext(GithubContext);
-  console.log(repos);
+  const { repos } = React.useContext(GithubContext);
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = {label:language, value:1};
+    } else {
+      total[language] = {...total[language],value: total[language].value+1}
+    }
+    return total
+  }, {})
+  languages = Object.values(languages).sort((a,b)=> {
+    return b.value - a.value
+  }).slice(0,5);
+
+  console.log(languages)
+
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13"
+    },
+    {
+      label: "CSS",
+      value: "23"
+    },
+    {
+      label: "Javascript",
+      value: "64"
+    }
+  ];
 
   return (
-    <h2>repos component</h2>
-    );
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={languages} />
+        <div></div>
+        <Doughnut2D data={chartData}/>
+        <div></div>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
